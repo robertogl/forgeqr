00a1b216e1e1ac6ba4c94586d6f643669b7fbfab
Roberto Valenti <robertogl.valenti@gmail.com>
2026-05-04 05:50:55 -0400

    Fix DB pooling for Supabase transaction pooler

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./qr_codes.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {},
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
